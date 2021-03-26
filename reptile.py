1f74cee459336887d99cdcc0386368492d6d49f7
Your Name <123465@gmail.com>
2021-03-26 16:03:27 +0800

    AA

--- reptile.py
+++ reptile.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-req=requests.get("http://210.70.80.21/-bs109021028/a.html")
+req=requests.get("http://isrc.ccs.asia.edu.tw/www/myjournal/myjournal.htm")
 req.encoding="utf8"
-soup=BeautifulSoup(req.text)
 
 if req.status_code==200:
-    #print(req.text)
-    result=soup.find_all("li")
-    print(result)
+    print(req.text)
+    soup=BeautifulSoup(req.text, "lxml")
+    print(soup)
+    result=soup.find_all("p")
+    fp=open("out2.txt","w",encoding="utf8")#big5
+    for val in result:
+        text1=val.text.replace("\n","")
+        text2=text1.replace("  ","")
+        print(text2)
+        fp.write(text2+"\n")
+    fp.close()
 else:
     print("no page")
-print(req.status_code)
