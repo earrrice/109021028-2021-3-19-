0ac852f7d6c6f7291f446f7c3230d266f8901e6b
earrrice <w10999999999@gmail.com>
2021-03-26 17:57:23 +0800

    OO

--- reptile.py
+++ reptile.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-req=requests.get("http://isrc.ccs.asia.edu.tw/www/myjournal/myjournal.htm")
-req.encoding="utf8"
+req=requests.get("http://dns2.asia.edu.tw/~jdwang/PaperList.htm")
+req.encoding="big5"
 
 if req.status_code==200:
     print(req.text)
     soup=BeautifulSoup(req.text, "lxml")
     print(soup)
-    result=soup.find_all("p")
-    fp=open("out2.txt","w",encoding="utf8")#big5
+    result=soup.find_all("li")
+    fp=open("out1.txt","w",encoding="big5")#big5
     for val in result:
         text1=val.text.replace("\n","")
         text2=text1.replace("  ","")
